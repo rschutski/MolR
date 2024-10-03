ad39c6c7e68b8adbb36e8fdb53c60bf98ef29218
r.schutski <r.schutski@insilicomedicine.com>
2024-10-03 00:54:51 +0000

    fixes

--- molr/interface.py
+++ molr/interface.py
@@ -1,13 +1,14 @@
 import pickle
 from pathlib import Path
-from typing import Any
+from typing import Any, Union
 
 import dgl
 import torch
+from datasets.formatting.formatting import LazyBatch, LazyRow
 from rdkit import Chem
 
-from data_processing import mol_to_dgl
-from model import GNN
+from molr.data_processing import mol_to_dgl
+from molr.model import GNN
 
 
 class MolRSmilesEmbedder:
@@ -16,20 +17,21 @@ class MolRSmilesEmbedder:
        Outputs zero vectors for invalid SMILES."""
     def __init__(
         self,
-        model_path: str | Path,
-        device: torch.device | None = None,
+        model_path: Union[str, Path],
+        device: Union[torch.device, None] = None,
     ):
         self.model_path = Path(model_path)
         self.device = device or torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.load_model(self.model_path, self.device)
 
-    def load_model(self, model_path: str | Path, device: torch.device) -> None:
+    def load_model(self, model_path: Union[str, Path], device: torch.device) -> None:
         self.feature_encoder = pickle.load(model_path.joinpath('feature_enc.pkl').open('rb'))
         self.hparams = pickle.load(model_path.joinpath('hparams.pkl').open('rb'))
         self.embedder = GNN(
             self.hparams['gnn'], self.hparams['layer'],
             self.hparams['feature_len'], self.hparams['dim'])
         self.dim = self.hparams['dim']
+
         if torch.cuda.is_available():
             self.embedder.load_state_dict(
                 torch.load(
@@ -45,36 +47,21 @@ class MolRSmilesEmbedder:
 
 
     def __call__(
-        self, batch: dict[str, Any], indices: list[int] | None = None, rank: int | None = None
+        self,
+        data: dict[str, Any],
     ) -> dict[str, Any]:
         """Transforms SMILES into an embedding"""
-        device = next(self.embedder.parameters()).device
-        graphs = [self.smiles_to_dgl_rdkit(
-            smiles, feature_encoder=self.feature_encoder) for smiles in batch['smiles']]
-        valid_indices = [i for i, graph in enumerate(graphs) if graph is not None]
-        graphs_gpu = [graph.to(device) for graph in graphs if graph is not None]
-        with torch.no_grad():
-            vectors = self.embedder(dgl.batch(graphs_gpu))
-            if len(valid_indices) < len(graphs):
-                vectors_full = torch.zeros((len(graphs), self.dim), device=device)
-                vectors_full[valid_indices] = vectors
-                batch['vector'] = vectors_full
-            else:
-                batch['vector'] = vectors
-        return batch
+        return self.process_hf_dataset(data)
 
-    def process_huggingface_dataset(
+    def process_hf_dataset(
         self,
         batch: dict[str, Any],
-        indices: list[int] | None = None,
-        rank: int | None = None,
+        indices: Union[list[int], None] = None,
+        rank: Union[int, None] = None,
     ) -> dict[str, Any]:
         """Transforms SMILES into an embedding"""
         device = next(self.embedder.parameters()).device
-        graphs = [
-            self.smiles_to_dgl_rdkit(smiles, feature_encoder=self.feature_encoder)
-            for smiles in batch['smiles']
-        ]
+        graphs = self.smiles_batch_to_dgl(batch, feature_encoder=self.feature_encoder)
         valid_indices = [i for i, graph in enumerate(graphs) if graph is not None]
         graphs_gpu = [graph.to(device) for graph in graphs if graph is not None]
         with torch.no_grad():
@@ -88,7 +75,9 @@ class MolRSmilesEmbedder:
         return batch
 
     @staticmethod
-    def smiles_to_dgl_rdkit(smiles: str, feature_encoder: dict[str, Any]) -> dgl.DGLGraph | None:
+    def smiles_to_dgl(
+        smiles: str, feature_encoder: dict[str, Any]
+    ) -> Union[dgl.DGLGraph, None]:
         try:
             mol = Chem.RemoveHs(Chem.MolFromSmiles(smiles))
             graph = mol_to_dgl(mol, feature_encoder)
@@ -96,3 +85,28 @@ class MolRSmilesEmbedder:
             graph = None
             print(f'Error in smiles_to_dgl_rdkit: {e}')
         return graph
+
+    @staticmethod
+    def smiles_batch_to_dgl(
+        batch: dict[str, Any],
+        feature_encoder: dict[str, Any],
+    ) -> Union[dgl.DGLGraph, list[dgl.DGLGraph], None]:
+        if isinstance(batch, LazyBatch):
+            graphs = [
+                MolRSmilesEmbedder.smiles_to_dgl(smiles, feature_encoder=feature_encoder)
+                for smiles in batch['smiles']
+            ]
+        elif isinstance(batch, LazyRow) or isinstance(batch, dict[str, Any]):
+            graphs = [
+                MolRSmilesEmbedder.smiles_to_dgl(
+                    batch['smiles'], feature_encoder=feature_encoder
+                )
+            ]
+        else:
+            raise ValueError('Invalid inputs type')
+
+        return graphs
+
+    @property
+    def dimension(self) -> int:
+        return self.dim
